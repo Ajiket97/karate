1264ac0a384a0c569e99adb41daf827d37e840fb
Ajiket97 <ajitshingnare02@gmail.com>
2024-06-22 16:01:19 +0530

    test case added

---
+++ src/test/java/com/testCases/DeleteFromGorest.java
@@ -0,0 +1,34 @@
+package com.testCases;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+
+import org.testng.annotations.Test;
+
+import com.pojoClass.GoRestUser;
+import com.util.SpecificationBuilder;
+
+import io.restassured.http.ContentType;
+
+public class DeleteFromGorest {
+	@Test
+	public void deleteDataGoRest() {
+		SpecificationBuilder sb = new SpecificationBuilder();
+		GoRestUser user  = new GoRestUser();
+		user.setId(55);
+		user.setGender("male");
+		user.setEmail("aj@1235.com");
+		user.setName("ajit");
+		user.setStatus("active");
+		given(sb.requestSB())
+		.body(user)
+		//.pathParam("id", "6940124")
+		.when()
+		.post()
+		.then()
+		.log()
+		.all();
+
+	}
+
+}
